a9f4cc8c76902f73aa636682656b5ddeacd65d38
Todd Smith <tsmith90@my.westga.edu>
2021-01-11 12:47:35 +0800

    Added labels and textboxs to LoginForm.

---
+++ TechSupport/View/LoginForm.Designer.cs
@@ -0,0 +1,124 @@
+﻿
+namespace TechSupport
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.userNameLabel = new System.Windows.Forms.Label();
+            this.passwordLabel = new System.Windows.Forms.Label();
+            this.errorsLabel = new System.Windows.Forms.Label();
+            this.userTextBox = new System.Windows.Forms.TextBox();
+            this.passwordTextBox = new System.Windows.Forms.TextBox();
+            this.errorTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // userNameLabel
+            // 
+            this.userNameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.userNameLabel.Location = new System.Drawing.Point(66, 42);
+            this.userNameLabel.Name = "userNameLabel";
+            this.userNameLabel.Size = new System.Drawing.Size(100, 50);
+            this.userNameLabel.TabIndex = 0;
+            this.userNameLabel.Text = "Username: ";
+            this.userNameLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.userNameLabel.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // passwordLabel
+            // 
+            this.passwordLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.passwordLabel.Location = new System.Drawing.Point(66, 124);
+            this.passwordLabel.Name = "passwordLabel";
+            this.passwordLabel.Size = new System.Drawing.Size(100, 50);
+            this.passwordLabel.TabIndex = 99;
+            this.passwordLabel.Text = "Password:";
+            this.passwordLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // errorsLabel
+            // 
+            this.errorsLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.errorsLabel.Location = new System.Drawing.Point(66, 211);
+            this.errorsLabel.Name = "errorsLabel";
+            this.errorsLabel.Size = new System.Drawing.Size(100, 50);
+            this.errorsLabel.TabIndex = 99;
+            this.errorsLabel.Text = "Error Message:";
+            this.errorsLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.errorsLabel.Click += new System.EventHandler(this.errorsLabel_Click);
+            // 
+            // userTextBox
+            // 
+            this.userTextBox.Location = new System.Drawing.Point(172, 59);
+            this.userTextBox.Name = "userTextBox";
+            this.userTextBox.Size = new System.Drawing.Size(199, 20);
+            this.userTextBox.TabIndex = 0;
+            // 
+            // passwordTextBox
+            // 
+            this.passwordTextBox.Location = new System.Drawing.Point(172, 141);
+            this.passwordTextBox.Name = "passwordTextBox";
+            this.passwordTextBox.Size = new System.Drawing.Size(199, 20);
+            this.passwordTextBox.TabIndex = 1;
+            this.passwordTextBox.UseSystemPasswordChar = true;
+            // 
+            // errorTextBox
+            // 
+            this.errorTextBox.Location = new System.Drawing.Point(172, 228);
+            this.errorTextBox.Name = "errorTextBox";
+            this.errorTextBox.ReadOnly = true;
+            this.errorTextBox.Size = new System.Drawing.Size(199, 20);
+            this.errorTextBox.TabIndex = 0;
+            this.errorTextBox.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 361);
+            this.Controls.Add(this.errorTextBox);
+            this.Controls.Add(this.passwordTextBox);
+            this.Controls.Add(this.userTextBox);
+            this.Controls.Add(this.errorsLabel);
+            this.Controls.Add(this.passwordLabel);
+            this.Controls.Add(this.userNameLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label userNameLabel;
+        private System.Windows.Forms.Label passwordLabel;
+        private System.Windows.Forms.Label errorsLabel;
+        private System.Windows.Forms.TextBox userTextBox;
+        private System.Windows.Forms.TextBox passwordTextBox;
+        private System.Windows.Forms.TextBox errorTextBox;
+    }
+}
+
--- TechSupport/View/LoginForm.cs
+++ TechSupport/View/LoginForm.cs
@@ -16,5 +16,15 @@ namespace TechSupport
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void errorsLabel_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
