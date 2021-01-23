c684232027873b0a5d7cf951374e7a6ea8756cac
tsmith90 <tsmith90@my.westga.edu>
2021-01-23 18:17:14 +0800

    Added comments to SearchIncidentDialog class.

--- TechSupport/View/SearchIncidentDialog.cs
+++ TechSupport/View/SearchIncidentDialog.cs
@@ -1,22 +1,24 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace TechSupport.View
 {
+    /// <summary>
+    /// Class used to search all Incidents according to the customerID  
+    /// </summary>
     public partial class SearchIncidentDialog : Form
     {
+        /// <summary>
+        /// 0-parameter constructor for the SearchIncidentDialog class  
+        /// </summary>
         public SearchIncidentDialog()
         {
             InitializeComponent();
         }
 
+        /// <summary>
+        /// Method to handle the cancel search incident button event  
+        /// </summary>
         private void cancelSearchButton_Click(object sender, EventArgs e)
         {
             this.DialogResult = DialogResult.Cancel;
