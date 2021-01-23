2c9a74b93699e5352b71c128524c8646fd23b11f
tsmith90 <tsmith90@my.westga.edu>
2021-01-23 15:05:14 +0800

    Added XML comments to the LoginForm.

--- TechSupport/View/LoginForm.cs
+++ TechSupport/View/LoginForm.cs
@@ -3,18 +3,27 @@ using System.Windows.Forms;
 using TechSupport.View;
 
 namespace TechSupport
-
+{   
+    /// <summary>
+    /// Class to represent a login form   
+    /// </summary>
     public partial class LoginForm : Form
     {
         public static string UserName { get; set; }
         public static MainForm mainForm;
 
+        /// <summary>
+        /// 0-parameter constructor for the LoginForm object  
+        /// </summary>
         public LoginForm()
         {
             InitializeComponent();
             mainForm = new MainForm();
         }
 
+        /// <summary>
+        /// Method to trigger event when LoginButton is used  
+        /// </summary>
         private void LoginButtonClick(object sender, EventArgs e)
         {
             if ((userTextBox.Text == "Jane") &&
@@ -30,11 +39,17 @@ namespace TechSupport
             }
         }
 
+        /// <summary>
+        /// Clears the error text label when the user makes a change
+        /// </summary>
         private void ClearErrors(object sender, EventArgs e)
         {
             errorLabelBox.Text = "";
         }
 
+        /// <summary>
+        /// Method to exit the application when the form is closed   
+        /// </summary>
         private void LoginForm_FormClosed(object sender, FormClosedEventArgs e)
         {
             Application.Exit();
