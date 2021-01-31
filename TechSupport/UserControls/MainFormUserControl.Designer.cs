b6b04cdbe41fa73009d395cd8f29f274a93ab333
tsmith90 <tsmith90@my.westga.edu>
2021-01-31 13:08:36 +0800

    Added MainFormUserControl.

---
+++ TechSupport/UserControls/MainFormUserControl.Designer.cs
@@ -0,0 +1,135 @@
+﻿
+namespace TechSupport.UserControls
+{
+    partial class MainFormUserControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.logoutLink = new System.Windows.Forms.LinkLabel();
+            this.nameLabel = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.addIncidentButton = new System.Windows.Forms.Button();
+            this.incidentDataGridView = new System.Windows.Forms.DataGridView();
+            this.searchIncidentButton = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.incidentDataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // logoutLink
+            // 
+            this.logoutLink.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.logoutLink.Location = new System.Drawing.Point(367, 80);
+            this.logoutLink.Name = "logoutLink";
+            this.logoutLink.Size = new System.Drawing.Size(100, 30);
+            this.logoutLink.TabIndex = 11;
+            this.logoutLink.TabStop = true;
+            this.logoutLink.Text = "Logout";
+            this.logoutLink.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // nameLabel
+            // 
+            this.nameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nameLabel.Location = new System.Drawing.Point(367, 0);
+            this.nameLabel.Name = "nameLabel";
+            this.nameLabel.Size = new System.Drawing.Size(100, 80);
+            this.nameLabel.TabIndex = 10;
+            this.nameLabel.Text = "Username";
+            this.nameLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 39.28571F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 60.71429F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 105F));
+            this.tableLayoutPanel1.Controls.Add(this.nameLabel, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.logoutLink, 2, 1);
+            this.tableLayoutPanel1.Controls.Add(this.addIncidentButton, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.incidentDataGridView, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.searchIncidentButton, 1, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(3, 3);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 75F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(470, 320);
+            this.tableLayoutPanel1.TabIndex = 12;
+            // 
+            // addIncidentButton
+            // 
+            this.addIncidentButton.Location = new System.Drawing.Point(3, 3);
+            this.addIncidentButton.Name = "addIncidentButton";
+            this.addIncidentButton.Size = new System.Drawing.Size(137, 56);
+            this.addIncidentButton.TabIndex = 7;
+            this.addIncidentButton.Text = "Add Incident";
+            this.addIncidentButton.UseVisualStyleBackColor = true;
+            // 
+            // incidentDataGridView
+            // 
+            this.incidentDataGridView.AllowUserToAddRows = false;
+            this.incidentDataGridView.AllowUserToDeleteRows = false;
+            this.incidentDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.tableLayoutPanel1.SetColumnSpan(this.incidentDataGridView, 2);
+            this.incidentDataGridView.Location = new System.Drawing.Point(3, 83);
+            this.incidentDataGridView.Name = "incidentDataGridView";
+            this.incidentDataGridView.ReadOnly = true;
+            this.incidentDataGridView.Size = new System.Drawing.Size(306, 234);
+            this.incidentDataGridView.TabIndex = 4;
+            this.incidentDataGridView.TabStop = false;
+            // 
+            // searchIncidentButton
+            // 
+            this.searchIncidentButton.Location = new System.Drawing.Point(146, 3);
+            this.searchIncidentButton.Name = "searchIncidentButton";
+            this.searchIncidentButton.Size = new System.Drawing.Size(139, 56);
+            this.searchIncidentButton.TabIndex = 8;
+            this.searchIncidentButton.Text = "Search Incident";
+            this.searchIncidentButton.UseVisualStyleBackColor = true;
+            // 
+            // MainFormUserControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "MainFormUserControl";
+            this.Size = new System.Drawing.Size(476, 327);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.incidentDataGridView)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.LinkLabel logoutLink;
+        private System.Windows.Forms.Label nameLabel;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button addIncidentButton;
+        private System.Windows.Forms.DataGridView incidentDataGridView;
+        private System.Windows.Forms.Button searchIncidentButton;
+    }
+}
