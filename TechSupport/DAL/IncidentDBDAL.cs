2f099f11312907ab8774561cae75774049bacd94
tsmith90 <tsmith90@my.westga.edu>
2021-02-05 11:59:41 +0800

    Created IncidentDBDAL class and added GetConnection method.

---
+++ TechSupport/DAL/IncidentDBDAL.cs
@@ -0,0 +1,24 @@
+﻿using System.Data.SqlClient;
+
+namespace TechSupport.DAL
+{
+    /// <summary>
+    /// Class to establish a connection to the TechSupport DB
+    /// </summary>
+    class IncidentDBDAL
+    {
+        /// <summary>
+        /// Method to establish a connection to the TechSupport DB
+        /// </summary>
+        /// <returns>The connection to the database</returns>
+        public static SqlConnection GetConnection()
+        {
+            string connectionString =
+                "Data Source=localhost;Initial Catalog=TechSupport;" +
+                "Integrated Security=True";
+
+            SqlConnection connection = new SqlConnection(connectionString);
+            return connection;
+        }
+    }
+}
