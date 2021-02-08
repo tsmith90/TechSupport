133d85889edddda81dbc50a76fcf12b6826f5833
tsmith90 <tsmith90@my.westga.edu>
2021-02-08 11:44:58 +0800

    Removed unneeded variables in Incident class.

--- TechSupport/Model/Incident.cs
+++ TechSupport/Model/Incident.cs
@@ -5,21 +5,15 @@
     /// </summary>
     public class Incident
     {
-        public int IncidentID { get; set; }
-
         public int CustomerID { get; set; }
 
         public string CustomerName { get; set; }
 
         public string ProductCode { get; set; }
 
-        public int TechnicianID { get; set; }
-
         public string TechnicianName { get; set; }
 
         public string DateOpened { get; set; }
-
-        public string DateClosed { get; set; }
 
         public string Title { get; set; }
 
