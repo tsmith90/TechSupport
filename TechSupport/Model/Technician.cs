533889aa732e22a9fca60eaaf6e0820ea67d30c6
tsmith90 <tsmith90@my.westga.edu>
2021-03-13 06:33:59 +0800

    Added a Technician model class

---
+++ TechSupport/Model/Technician.cs
@@ -0,0 +1,17 @@
+﻿
+namespace TechSupport.Model
+{
+    /// <summary>
+    /// Model for the Technician object
+    /// </summary>
+    class Technician
+    {
+        public int TechnicianID { get; set; }
+
+        public string TechnicianName { get; set; }
+
+        public string TechnicianEmail { get; set; }
+
+        public string TechnicianPhone { get; set; }
+    }
+}
