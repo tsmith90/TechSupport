1ac9b22ac0b21d5ddfe0ccaea4f3c149f10b41b0
tsmith90 <tsmith90@my.westga.edu>
2021-03-13 16:12:32 +0800

    Removed old userControls from TechnicianUserControl.

--- TechSupport/UserControls/TechnicianIncidentsUserControl.Designer.cs
+++ TechSupport/UserControls/TechnicianIncidentsUserControl.Designer.cs
@@ -30,56 +30,15 @@ namespace TechSupport.UserControls
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            System.Windows.Forms.Label nameLabel;
-            System.Windows.Forms.Label emailLabel;
-            System.Windows.Forms.Label phoneLabel;
             this.techniciansBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.techSupportDataSet = new TechSupport.TechSupportDataSet();
-            this.emailTextBox = new System.Windows.Forms.TextBox();
-            this.phoneTextBox = new System.Windows.Forms.TextBox();
-            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
-            this.nameComboBox = new System.Windows.Forms.ComboBox();
             this.techniciansTableAdapter = new TechSupport.TechSupportDataSetTableAdapters.TechniciansTableAdapter();
             this.tableAdapterManager = new TechSupport.TechSupportDataSetTableAdapters.TableAdapterManager();
             this.techniciansBindingSource1 = new System.Windows.Forms.BindingSource(this.components);
-            nameLabel = new System.Windows.Forms.Label();
-            emailLabel = new System.Windows.Forms.Label();
-            phoneLabel = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.techniciansBindingSource)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.techSupportDataSet)).BeginInit();
-            this.tableLayoutPanel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.techniciansBindingSource1)).BeginInit();
             this.SuspendLayout();
-            // 
-            // nameLabel
-            // 
-            nameLabel.AutoSize = true;
-            nameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            nameLabel.Location = new System.Drawing.Point(22, 21);
-            nameLabel.Name = "nameLabel";
-            nameLabel.Size = new System.Drawing.Size(55, 20);
-            nameLabel.TabIndex = 3;
-            nameLabel.Text = "Name:";
-            // 
-            // emailLabel
-            // 
-            emailLabel.AutoSize = true;
-            emailLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            emailLabel.Location = new System.Drawing.Point(22, 60);
-            emailLabel.Name = "emailLabel";
-            emailLabel.Size = new System.Drawing.Size(52, 20);
-            emailLabel.TabIndex = 5;
-            emailLabel.Text = "Email:";
-            // 
-            // phoneLabel
-            // 
-            phoneLabel.AutoSize = true;
-            phoneLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            phoneLabel.Location = new System.Drawing.Point(22, 98);
-            phoneLabel.Name = "phoneLabel";
-            phoneLabel.Size = new System.Drawing.Size(59, 20);
-            phoneLabel.TabIndex = 7;
-            phoneLabel.Text = "Phone:";
             // 
             // techniciansBindingSource
             // 
@@ -90,60 +49,6 @@ namespace TechSupport.UserControls
             // 
             this.techSupportDataSet.DataSetName = "TechSupportDataSet";
             this.techSupportDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
-            // 
-            // emailTextBox
-            // 
-            this.emailTextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.techniciansBindingSource, "Email", true));
-            this.emailTextBox.Location = new System.Drawing.Point(92, 63);
-            this.emailTextBox.Name = "emailTextBox";
-            this.emailTextBox.ReadOnly = true;
-            this.emailTextBox.Size = new System.Drawing.Size(234, 20);
-            this.emailTextBox.TabIndex = 6;
-            // 
-            // phoneTextBox
-            // 
-            this.phoneTextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.techniciansBindingSource, "Phone", true));
-            this.phoneTextBox.Location = new System.Drawing.Point(92, 101);
-            this.phoneTextBox.Name = "phoneTextBox";
-            this.phoneTextBox.ReadOnly = true;
-            this.phoneTextBox.Size = new System.Drawing.Size(234, 20);
-            this.phoneTextBox.TabIndex = 8;
-            // 
-            // tableLayoutPanel1
-            // 
-            this.tableLayoutPanel1.ColumnCount = 3;
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 19F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 70F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 266F));
-            this.tableLayoutPanel1.Controls.Add(nameLabel, 1, 1);
-            this.tableLayoutPanel1.Controls.Add(phoneLabel, 1, 3);
-            this.tableLayoutPanel1.Controls.Add(this.phoneTextBox, 2, 3);
-            this.tableLayoutPanel1.Controls.Add(emailLabel, 1, 2);
-            this.tableLayoutPanel1.Controls.Add(this.emailTextBox, 2, 2);
-            this.tableLayoutPanel1.Controls.Add(this.nameComboBox, 2, 1);
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(4, 16);
-            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-            this.tableLayoutPanel1.RowCount = 5;
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 21F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 39F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 38F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 61F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 118F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(562, 496);
-            this.tableLayoutPanel1.TabIndex = 9;
-            // 
-            // nameComboBox
-            // 
-            this.nameComboBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.techniciansBindingSource, "Name", true));
-            this.nameComboBox.DataSource = this.techniciansBindingSource;
-            this.nameComboBox.DisplayMember = "Name";
-            this.nameComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.nameComboBox.FormattingEnabled = true;
-            this.nameComboBox.Location = new System.Drawing.Point(92, 24);
-            this.nameComboBox.Name = "nameComboBox";
-            this.nameComboBox.Size = new System.Drawing.Size(234, 21);
-            this.nameComboBox.TabIndex = 9;
-            this.nameComboBox.ValueMember = "TechID";
             // 
             // techniciansTableAdapter
             // 
@@ -165,14 +70,11 @@ namespace TechSupport.UserControls
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.tableLayoutPanel1);
             this.Name = "technicianIncidentsUserControl";
             this.Size = new System.Drawing.Size(592, 565);
             this.Load += new System.EventHandler(this.Form_Load);
             ((System.ComponentModel.ISupportInitialize)(this.techniciansBindingSource)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.techSupportDataSet)).EndInit();
-            this.tableLayoutPanel1.ResumeLayout(false);
-            this.tableLayoutPanel1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.techniciansBindingSource1)).EndInit();
             this.ResumeLayout(false);
 
@@ -184,10 +86,6 @@ namespace TechSupport.UserControls
         private System.Windows.Forms.BindingSource techniciansBindingSource;
         private TechSupportDataSetTableAdapters.TechniciansTableAdapter techniciansTableAdapter;
         private TechSupportDataSetTableAdapters.TableAdapterManager tableAdapterManager;
-        private System.Windows.Forms.TextBox emailTextBox;
-        private System.Windows.Forms.TextBox phoneTextBox;
-        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
-        private System.Windows.Forms.ComboBox nameComboBox;
         private System.Windows.Forms.BindingSource techniciansBindingSource1;
     }
 }
