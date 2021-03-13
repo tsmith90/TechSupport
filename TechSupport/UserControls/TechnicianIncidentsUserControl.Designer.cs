a9f496cd22524d1c0cf39148559b628c0f5a4247
tsmith90 <tsmith90@my.westga.edu>
2021-03-13 16:16:54 +0800

    Removed old controls from technicianIncidentsUserControl.

--- TechSupport/UserControls/TechnicianIncidentsUserControl.Designer.cs
+++ TechSupport/UserControls/TechnicianIncidentsUserControl.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace TechSupport.UserControls
 {
-    partial class technicianIncidentsUserControl
+    partial class TechnicianIncidentsUserControl
     {
         /// <summary> 
         /// Required designer variable.
@@ -29,42 +29,9 @@ namespace TechSupport.UserControls
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.techniciansBindingSource = new System.Windows.Forms.BindingSource(this.components);
-            this.techSupportDataSet = new TechSupport.TechSupportDataSet();
-            this.techniciansTableAdapter = new TechSupport.TechSupportDataSetTableAdapters.TechniciansTableAdapter();
-            this.tableAdapterManager = new TechSupport.TechSupportDataSetTableAdapters.TableAdapterManager();
-            this.techniciansBindingSource1 = new System.Windows.Forms.BindingSource(this.components);
-            ((System.ComponentModel.ISupportInitialize)(this.techniciansBindingSource)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.techSupportDataSet)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.techniciansBindingSource1)).BeginInit();
-            this.SuspendLayout();
-            // 
-            // techniciansBindingSource
-            // 
-            this.techniciansBindingSource.DataMember = "Technicians";
-            this.techniciansBindingSource.DataSource = this.techSupportDataSet;
-            // 
-            // techSupportDataSet
-            // 
-            this.techSupportDataSet.DataSetName = "TechSupportDataSet";
-            this.techSupportDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
-            // 
-            // techniciansTableAdapter
-            // 
-            this.techniciansTableAdapter.ClearBeforeFill = true;
-            // 
-            // tableAdapterManager
-            // 
-            this.tableAdapterManager.BackupDataSetBeforeUpdate = false;
-            this.tableAdapterManager.IncidentsTableAdapter = null;
-            this.tableAdapterManager.TechniciansTableAdapter = this.techniciansTableAdapter;
-            this.tableAdapterManager.UpdateOrder = TechSupport.TechSupportDataSetTableAdapters.TableAdapterManager.UpdateOrderOption.InsertUpdateDelete;
-            // 
-            // techniciansBindingSource1
-            // 
-            this.techniciansBindingSource1.DataMember = "Technicians";
-            this.techniciansBindingSource1.DataSource = this.techSupportDataSet;
+ 
+
+
             // 
             // technicianIncidentsUserControl
             // 
@@ -72,20 +39,10 @@ namespace TechSupport.UserControls
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Name = "technicianIncidentsUserControl";
             this.Size = new System.Drawing.Size(592, 565);
-            this.Load += new System.EventHandler(this.Form_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.techniciansBindingSource)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.techSupportDataSet)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.techniciansBindingSource1)).EndInit();
             this.ResumeLayout(false);
-
         }
 
         #endregion
 
-        private TechSupportDataSet techSupportDataSet;
-        private System.Windows.Forms.BindingSource techniciansBindingSource;
-        private TechSupportDataSetTableAdapters.TechniciansTableAdapter techniciansTableAdapter;
-        private TechSupportDataSetTableAdapters.TableAdapterManager tableAdapterManager;
-        private System.Windows.Forms.BindingSource techniciansBindingSource1;
     }
 }
--- TechSupport/UserControls/TechnicianIncidentsUserControl.cs
+++ TechSupport/UserControls/TechnicianIncidentsUserControl.cs
@@ -3,31 +3,14 @@ using System.Windows.Forms;
 
 namespace TechSupport.UserControls
 {
-    public partial class technicianIncidentsUserControl : UserControl
+    public partial class TechnicianIncidentsUserControl : UserControl
     {
-        public technicianIncidentsUserControl()
+        public TechnicianIncidentsUserControl()
         {
             InitializeComponent();
         }
 
-        private void techniciansBindingNavigatorSaveItem_Click(object sender, EventArgs e)
-        {
-            this.Validate();
-            this.techniciansBindingSource.EndEdit();
-            this.tableAdapterManager.UpdateAll(this.techSupportDataSet);
 
-        }
-
-        private void Form_Load(object sender, EventArgs e)
-        {
-            Fill_Form();
-        }
-
-        public void Fill_Form()
-        {
-
-            this.techniciansTableAdapter.Fill(this.techSupportDataSet.Technicians);
-        }
     
     }
 }
